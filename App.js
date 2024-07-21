1761fa01e634c9fb8a717f2e972ed7c691c354df
Your Name <you@example.com>
2024-07-21 20:51:33 +0530

    hello

---
+++ App.js
@@ -0,0 +1,18 @@
+const heading = React.createElement("div",{id:"parent"},
+   [ React.createElement("div",{id:"child"},
+    [
+        React.createElement("h1",{},"I m h1 tag"),
+        React.createElement("h2",{},"i m h2 tag"),
+    ]
+),
+React.createElement("div",{id:"child"},
+    [
+        React.createElement("h1",{},"I m h1 tag"),
+        React.createElement("h2",{},"i m h2 tag")
+    ]
+)]
+    
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
